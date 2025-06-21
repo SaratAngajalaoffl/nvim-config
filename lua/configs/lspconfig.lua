383ff37957663e8e891f126ff503578a73bf6d0a
mugiwara@archlinux <saratangajala@gmail.com>
2025-06-21 15:45:37 +0530

    adds shortcut for split window and lsp for json and xml

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,8 +1,10 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls"}
+local servers = { "html", "cssls", "json" }
 vim.lsp.enable(servers)
 
 local lspconfig = require('lspconfig')-- read :h vim.lsp.config for changing options of lsp servers 
+lspconfig.jsonls.setup({})
 lspconfig.ts_ls.setup({})
 lspconfig.pyright.setup({})
+lspconfig.lemminx.setup({})
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,9 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<C-k>", function()
+  vim.cmd("vsplit %")
+end, { desc = "Split screen vertically" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
